5327dfd3ee117659aadad01394d9bafa662b8011
justin <jtr342@gmail.com>
2022-10-04 19:14:40 -0700

    added string solution

--- reverse_integer.py
+++ reverse_integer.py
@@ -12,3 +12,19 @@ class Solution:
         if ret > (2**31 - 1): return 0
         if ret < (-2**31): return 0
         return ret * sign
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = 1
+        if x < 0:
+            sign = -1
+            x = -1 * x
+        s = str(x)
+        reverse = s[::-1]
+        i = int(reverse)
+        i *= sign
+        if i > (2**31 - 1): return 0
+        if i < (-2**31): return 0
+        return i
+        
+        
